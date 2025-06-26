45950eeb4babbaaf072c7a37b9a57a228817e505
riel-fas <riadelfassi17@gmail.com>
2025-06-26 12:29:54 +0100

    fixing heredoc
    switching the heredoc to fork

--- src/env/copy_env.c
+++ src/env/copy_env.c
@@ -3,26 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   copy_env.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riel-fas <riel-fas@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 17:50:00 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/06/20 18:26:54 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/06/26 12:28:23 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/mini_shell.h"
 
-/**
- * @brief Kad i9ssem l'environment variable l smiya o l9ima
- *
- * Had fonction katredd string dyal environment variable (bhal "PATH=/bin:/usr/bin")
- * o kat9essmo l key o value. Kat9elleb 3la awel '=' bach t3ref fin ghadi
- * t9essem string. Ila mal9at '=', ghadi tkhelli l value khawya.
- *
- * @param env_str String dyal variable li bghina n9essmo
- * @param key Pointer fash ghadi tkhebbi smiyat l variable (khassek dir free 3liha men be3d)
- * @param value Pointer fash ghadi tkhebbi l9ima dyal variable (khassek dir free 3liha men be3d)
- */
 static void	parse_env_var(char *env_str, char **key, char **value)
 {
 	int	i;
@@ -37,20 +26,6 @@ static void	parse_env_var(char *env_str, char **key, char **value)
 		*value = ft_strdup("");
 }
 
-/**
- * @brief Kat khle9 linked list men array dyal environment variables
- *
- * Had fonction kat7ewel array dyal environment (envp) l wa7ed linked list
- * dyal t_env structures bash ykon s'hel manipulation dyalo. Katdour 3la kol
- * string fl env, o kat9essmo b parse_env_var() l key o value, o katkhle9
- * nodes jdad b new_env_node(), o tzid hom f linked list b add_env_node().
- *
- * Had fonction kat-allouer memory l kol node, key, o value, o khass dir free
- * 3lihom melli kay salew l'utilisation.
- *
- * @param env Array dyal environment li bghina n7ewloh
- * @return t_env* Ras linked list dyal environment jdid, wla NULL ila kan moshkil
- */
 t_env	*create_env_list(char **env)
 {
 	t_env	*env_list;
@@ -75,16 +50,6 @@ t_env	*create_env_list(char **env)
 	return (env_list);
 }
 
-/**
- * @brief Kat jbed smiyat l'utilisateur men l'environment
- *
- * Had fonction kat9elleb 3la variable "USER" f environment.
- * Ila l9ato, katrejje3 copy dyalo. Ila mal9ato, katrejje3 "user".
- * Li kayst3mel had fonction khasso idir free 3la string li katrejje3.
- *
- * @param env Linked list dyal environment fash ghadi n9ellbo
- * @return char* String jdida fiha smiyat l'utilisateur
- */
 char	*get_username(t_env *env)
 {
 	char	*username;
@@ -97,21 +62,6 @@ char	*get_username(t_env *env)
 	return (username);
 }
 
-/**
- * @brief Kat9essem variable PATH l directory bwahdhom
- *
- * Had fonction katjbed l variable PATH men environment o kat9essmo
- * l array dyal strings, kol string fiha directory men PATH. PATH
- * kay t9essem 3end kol caractère deux points (':'). Ila makansh
- * PATH fl environment, katrejje3 NULL.
- *
- * Li kayst3mel had fonction khasso idir free 3la array li rejj3at
- * o 3la kol string fiha.
- *
- * @param env Linked list dyal environment fash ghadi n9ellbo
- * @return char** Array dyal strings fiha directories dyal PATH,
- *                wla NULL ila mal9inash PATH
- */
 char	**split_paths(t_env *env)
 {
 	char	*path_str;
--- src/env/env_list.c
+++ src/env/env_list.c
@@ -3,26 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   env_list.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riel-fas <riel-fas@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 18:53:58 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/06/20 18:31:41 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/06/26 12:28:54 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../includes/mini_shell.h"
 
-/**
- * @brief Kat khle9 node jdid l environment variable
- *
- * Had fonction katkhedem node jdid dyal t_env structures bash tkhezzen
- * fiha variable dyal environment. Katdir malloc l node o kat-dupliquer
- * name o value dyalo. Ila kan moshkil f malloc, katrejje3 NULL.
- *
- * @param name Smiyat l variable (key)
- * @param value L9ima dyal l variable
- * @return t_env* Node jdida, wla NULL ila kan moshkil f malloc
- */
 t_env	*new_env_node(char *name, char *value)
 {
 	t_env	*new;
@@ -36,17 +25,6 @@ t_env	*new_env_node(char *name, char *value)
 	return (new);
 }
 
-/**
- * @brief Katzid node jdida f l'ekher dyal linked list
- *
- * Had fonction katzid new_node f l'ekher dyal env_list. Ila kan
- * env_list khawi (NULL), kat 3ayet l new_node howa l'awel dyal list.
- * Sinon, katdour 7ta l l'ekher dyal list o katzid fiha new_node.
- *
- * @param env_list Pointer l ras linked list (double pointer bash n9edro
- *                 nbeddlo pointer f cas ila kan khawi)
- * @param new_node Node jdida li bghina nzido f list
- */
 void	add_env_node(t_env **env_list, t_env *new_node)
 {
 	t_env	*temp;
@@ -62,17 +40,6 @@ void	add_env_node(t_env **env_list, t_env *new_node)
 	temp->next = new_node;
 }
 
-/**
- * @brief Kat9elleb 3la variable dyal environment b key dyalo
- *
- * Had fonction katdour 3la linked list dyal environment o kat9aren
- * keys m3a key li 3tinaha. Ila l9at match, katrejje3 node kamla.
- * Ila mal9at walo, katrejje3 NULL.
- *
- * @param env Linked list dyal environment fash ghadi n9ellbo
- * @param key Smiyat l variable li kan9ellbo 3liha
- * @return t_env* Node li 3endha nefs key, wla NULL ila mal9atha
- */
 t_env	*find_env_var(t_env *env, char *key)
 {
 	t_env	*current;
@@ -87,16 +54,6 @@ t_env	*find_env_var(t_env *env, char *key)
 	return (NULL);
 }
 
-/**
- * @brief Katjbed l9ima dyal variable dyal environment b key dyalo
- *
- * Had fonction kat3ayet l find_env_var bash tl9a node li fiha key
- * li bghina, o katrejje3 ghir value dyalha. Ila mal9at node, katrejje3 NULL.
- *
- * @param env Linked list dyal environment
- * @param key Smiyat l variable li bghina value dyalha
- * @return char* Value dyal variable, wla NULL ila mal9atha
- */
 char	*get_env_value(t_env *env, char *key)
 {
 	t_env	*var;
@@ -107,17 +64,6 @@ char	*get_env_value(t_env *env, char *key)
 	return (NULL);
 }
 
-/**
- * @brief Kat7ewel linked list dyal env l array dyal strings bash nestekhdemha m3a execve
- *
- * Had fonction kat7eseb 9edash kayn men node f linked list, katdir malloc
- * l array dyal strings, o katkhdem strings bhal "KEY=VALUE" l kol variable.
- * Katkhessek dir free 3la array + kol string fih melli tsali ste3malo.
- * F l'ekher dyal array katzid NULL bash ykoun conforme m3a format envp.
- *
- * @param env Linked list dyal environment
- * @return char** Array dyal strings "KEY=VALUE", wla NULL ila kan moshkil f malloc
- */
 char	**env_list_to_array(t_env *env)
 {
 	t_env	*current;
